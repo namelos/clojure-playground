add59561916d6e0ed1496ec5d996f19feab10b4e
Yifan Wang <namelos@outlook.com>
2016-04-29 19:07:16 +0800

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject playground "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot playground.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
